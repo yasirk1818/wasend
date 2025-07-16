ca1f603c96d8df48bc5395a9f5d0d62f788b944f
yasirk1818 <yasirk1818@gmail.com>
2025-07-16 03:27:01 -0700

    Create auth.js

---
+++ routes/auth.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+const router = express.Router();
+
+// Register
+router.post('/register', async (req, res) => {
+    const { username, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = new User({ username, password: hashedPassword });
+    await user.save();
+    res.status(201).send('User registered');
+});
+
+// Login
+router.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    if (!user || !await bcrypt.compare(password, user.password)) {
+        return res.status(401).send('Invalid credentials');
+    }
+    const token = jwt.sign({ userId: user._id }, 'YOUR_SECRET_KEY');
+    res.json({ token });
+});
+
+module.exports = router;
